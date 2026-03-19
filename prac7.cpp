cece0b2498be51ed87ebe52fe0afd4a6ec10de36
WASDGENIUS <senshi-74@mail.ru>
2026-03-19 14:34:44 +0500

    prac 7 not done

---
+++ prac7.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+// void swal(float& a, float& b)
+// {
+//     a += b;
+//     b = a - b;
+//     a = a - b;
+
+// }
+
+// int main()
+// {
+//     float a, b;
+//     cin >> a >> b;
+//     swal(a, b);
+//     cout << a << " " << b << endl;
+//     return 0;
+
+// }
+
+
+
+// float f1(float x);
+// float f2(float x);
+// float f3(float x);
+// void print(float x, float (*y)(float));
+
+// int main()
+// {
+//     float x = 0.6f, y = 0.3f, z = 0.1f;
+//     cout << "f1=";
+//     print(x, f1);
+//     cout << "f2=" ;
+//     print(y, f2);
+//     cout << "f3=" ;
+//     print(z, f3);
+//     float r = f1(x) + f2(y) * f3(f2(z));
+//     cout << r << endl;
+
+//     return 0;
+// }
+
+// float f1(float x)
+// {
+//     return sin(x) / x;
+// }
+
+// float f2(float x)
+// {
+//     return x * x + 2 * pow(x, 1.5);
+// }
+
+// float f3(float x)
+// {
+//     return (x+2) * (x+1) * x;
+// }
+
+// void print(float x, float (*y)(float))
+// {
+//     cout << y(x) << endl;
+// }
+
+
